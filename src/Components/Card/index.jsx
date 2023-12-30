a0a5cf045aca713d10b6dc6d20b04535f4957828
Camilo Guzman <camiloge_91@hotmail.com>
2023-12-29 23:06:58 -0500

    Maquetación_productDetail

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -3,13 +3,22 @@ import { ShoppingCartContext } from '../../Context'
 
 const Card = (data) => {
   const context = useContext(ShoppingCartContext)
+  const justOnClick = (event) =>{
+    event.stopPropagation();
+    context.setCount(context.count + 1);
+  }
+
   return (
-    <div className='bg-white cursor-pointer w-56 h-60 rounded-lg'>
+    <div 
+      className='bg-white cursor-pointer w-56 h-60 rounded-lg'
+      onClick={() => context.openProductDetail()}>
       <figure className='relative mb-2 w-full h-4/5'>
         <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>{data.data.category}</span>
         <img className='w-full h-full object-cover rounded-lg' src={data.data.image} alt={data.data.title} />
-        <button className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1' onClick={() => context.setCount(context.count + 1)}>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+        <button 
+          className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1' 
+          onClick={justOnClick}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"   className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
           </svg>
         </button>
