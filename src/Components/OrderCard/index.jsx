16e8b7f316ba3b59e8965b06073024b161a824d0
Camilo Guzman <camiloge_91@hotmail.com>
2024-01-03 13:13:03 -0500

    Checkout_productos_carrito

--- src/Components/OrderCard/index.jsx
+++ src/Components/OrderCard/index.jsx
@@ -2,6 +2,13 @@ import PropTypes from 'prop-types'
 
 const OrderCard = props => {
   const { id, title, imageUrl, price, handleDelete } = props
+  let renderCloseIcon
+  if (handleDelete) {
+    renderCloseIcon = 
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 cursor-pointer text-white" onClick={() => handleDelete(id)}>
+      <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+    </svg>
+  }
 
   //Para quitar el error ' ' is missing in props validation
   OrderCard.propTypes = {
@@ -23,9 +30,7 @@ const OrderCard = props => {
       <div className='flex items-center gap-2'>
         <p className='text-lg font-medium'>{price}</p>
         <button className=' bg-red-600 w-5 h-5 rounded-full '>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 cursor-pointer text-white" onClick={() => handleDelete(id)}>
-            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-          </svg>
+          {renderCloseIcon}
         </button>
       </div>
     </div>
--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -1,10 +1,27 @@
+import { useContext } from 'react'
+import { ShoppingCartContext } from '../../Context'
 import Layout from "../../Components/Layout"
+import OrderCard from '../../Components/OrderCard'
 
 function MyOrder() {
-
+  const context = useContext(ShoppingCartContext)
   return (
     <Layout>
       My Order
+      <div className='px-6 flex-1'>
+        {
+          context.order && context.order.length > 0 ? context.order.slice(-1)[0].products.map(product => (
+            <OrderCard 
+              key={product.id}
+              id={product.id}
+              title={product.title} 
+              imageUrl={product.image} 
+              price={product.price} 
+            />
+          ))
+        : <p>There is no product.</p>
+        }
+      </div>
     </Layout>
   )
 }
