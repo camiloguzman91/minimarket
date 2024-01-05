f33d5d34176337b405da78ddf8228672b31dcf08
Camilo Guzman <camiloge_91@hotmail.com>
2024-01-04 21:41:51 -0500

    Órdenes_compra

--- src/Components/OrdersCard/index.jsx
+++ src/Components/OrdersCard/index.jsx
@@ -10,12 +10,19 @@ const OrdersCard = props => {
   }
 
   return (
-    <div className='flex justify-between items-center mb-3 border border-black'>
-      <p>
-        <span>03/01/23</span>
-        <span>{totalProducts}</span>
-        <span>{totalPrice}</span>
-      </p>
+    <div className='flex justify-between items-center mb-3 border border-black rounded-lg p-4 w-80'>
+      <div className='flex justify-between w-full'>
+        <p className='flex flex-col'>
+          <span className='font-light'>03/01/23</span>
+          <span className='font-light'>{totalProducts} articles</span>
+        </p>
+        <p className='flex items-center gap-2'>
+          <span className='font-medium text-2xl'>${totalPrice}</span>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-black cursor-pointer">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+          </svg>
+        </p>
+      </div>
     </div>
   )
 }
