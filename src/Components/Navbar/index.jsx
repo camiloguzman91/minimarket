edf6d2aa83b06347c841e375f15bd2ccf95152a6
Camilo Guzman <camiloge_91@hotmail.com>
2024-01-06 18:40:24 -0500

    Filtro_categorias

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -11,12 +11,13 @@ const Navbar = () => {
       <ul className='flex items-center gap-3'>
         <li className='font-semibold text-lg'>
           <NavLink to='/'>
-            Shop-c
+            Minimarket
           </NavLink>
         </li>
         <li>
           <NavLink 
             to='/'
+            onClick={() => context.setSearchByCategory()}
             className={({ isActive }) =>
               isActive ? activeStyle : undefined
             }>
@@ -25,16 +26,8 @@ const Navbar = () => {
         </li>
         <li>
           <NavLink 
-            to='/clothes'
-            className={({ isActive }) =>
-              isActive ? activeStyle : undefined
-            }>
-            Clothes
-          </NavLink>
-        </li>
-        <li>
-          <NavLink 
             to='/electronics'
+            onClick={() => context.setSearchByCategory('electronics')}
             className={({ isActive }) =>
               isActive ? activeStyle : undefined
             }>
@@ -43,29 +36,32 @@ const Navbar = () => {
         </li>
         <li>
           <NavLink 
-            to='/fornitures'
+            to='/jewelery'
+            onClick={() => context.setSearchByCategory('jewelery')}
             className={({ isActive }) =>
               isActive ? activeStyle : undefined
             }>
-            Fornitures
+            Jewelery
           </NavLink>
         </li>
         <li>
           <NavLink 
-            to='/toys'
+            to="/men's clothing"
+            onClick={() => context.setSearchByCategory("men's clothing")}
             className={({ isActive }) =>
               isActive ? activeStyle : undefined
             }>
-            Toys
+            Men&apos;s clothing
           </NavLink>
         </li>
         <li>
           <NavLink 
-            to='/others'
+            to="/women's clothing"
+            onClick={() => context.setSearchByCategory("women's clothing")}
             className={({ isActive }) =>
               isActive ? activeStyle : undefined
             }>
-            Others
+            Women&apos;s clothing
           </NavLink>
         </li>
       </ul>
@@ -92,7 +88,7 @@ const Navbar = () => {
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
           </svg>
-          <div>{context.count}</div>
+          <div>{context.cartProducts.length}</div>
         </li>
       </ul>
     </nav>
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -9,23 +9,16 @@ function Home() {
   const context = useContext(ShoppingCartContext)
 
   const renderView = () => {
-    if (context.searchByTitle?.length > 0) {
-      if (context.filteredItems?.length > 0) {
-        return (
-          context.filteredItems?.map(item => (
-            <Card key={item.id} data={item} />
-          ))
-        )
-      } else {
-        return (
-          <div>Nothing to show</div>
-        )
-      }
-    } else {
+
+    if (context.filteredItems?.length > 0) {
       return (
-        context.items?.map(item => (
+        context.filteredItems?.map(item => (
           <Card key={item.id} data={item} />
         ))
+      )
+    } else {
+      return (
+        <div>Nothing to show</div>
       )
     }
   }
