263d398485b382df5619f90017adada8da64d8aa
Camilo Guzman <camiloge_91@hotmail.com>
2024-01-05 19:29:38 -0500

    Buscador_productos

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
+import { apiUrl } from '../api'
 
 export const ShoppingCartContext = createContext()
 
@@ -26,6 +27,18 @@ export const ShoppingCartProvider = ({children}) => {
   //Shopping Cart - Order
   const [order, setOrder] = useState([])
 
+  //Get products
+  const [items, setItems] = useState(null)
+
+   //Get products by title
+  const [searchByTitle, setSearchByTitle] = useState(null)
+
+  useEffect(() => {
+    fetch(`${apiUrl}`)
+      .then(response => response.json())
+      .then(data => setItems(data))
+  }, [])
+
   return (
     <ShoppingCartContext.Provider value={{
       count,
@@ -41,7 +54,11 @@ export const ShoppingCartProvider = ({children}) => {
       openCheckoutSideMenu,
       closeCheckoutSideMenu,
       order,
-      setOrder
+      setOrder,
+      items,
+      setItems,
+      searchByTitle,
+      setSearchByTitle
     }}>
       {children}
     </ShoppingCartContext.Provider>
